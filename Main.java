7f95b7932d2b2d82be044d9aa136f781657a290c
Alinur-mus <148569986+Alinur-mus@users.noreply.github.com>
2023-10-27 19:13:38 +0600

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,123 @@
+// Press Shift twice to open the Search Everywhere dialog and type `show whitespaces`,
+// then press Enter. You can now see whitespace characters in your code.
+public class Main {
+    public static void main(String[] args) {
+
+//        lesson 5
+//        1 тапсырма шаршы периметри
+//        int a = 4;
+//        int p = 4*a;
+//        System.out.println("Шаршы периметри: " + p);
+
+//        2 тапсырма
+//        double a = 6;
+//        double b = 7;
+//        double arifmet = (a+b)/2;
+//        double geomet = Math.sqrt(a*b);
+//        System.out.println("Арифметикалык ортасы: " + arifmet);
+//        System.out.println("Геометриялык ортасы: " + geomet);
+
+//        3 тапсырма
+//        double a = 9;
+//        double b = 10;
+//        double kosu = a+b;
+//        double azaitu = a-b;
+//        double kobeitu = a*b;
+//        double bolu = a/b;
+//        System.out.println("Косындысы: " +kosu+"\n"+ "Айырмасы: " +azaitu+"\n"+ "Кобейтиндиси: "+ kobeitu+"\n"+ "Биринши санды екинши санга болиндиси: "+bolu);
+
+//        4 Тапсырма
+//int a = 234/7;
+//System.out.println(a);
+
+//        5 Тапсырма
+//        int a = 65;
+//        int ondyk = a/10;
+//        int birlik = a%10;
+//        System.out.println("Циврларынын косындысы: "+ ondyk+birlik+"\n"+"Цифрларының көбейтіндісі: " + ondyk*birlik);
+//
+//
+//        6 Тапсырма
+//        int a = 476;
+//        int juz = a/100;
+//        int ondik = a/10%10;
+//        int birlrk = a%10;
+//        System.out.println("ондағы ондықтардың саны: "+ juz+"\n"+"ондағы ондык саны: "+ondik+"\n"+"ондағы бірліктердің саны: "+birlrk);
+//
+//
+
+//        7 Тапсырма
+
+//        int a = 476;
+//        int juz = a/100;
+//        int ondik = a/10%10;
+//       int birlrk = a%100%10;
+//       int kosyndy = juz+ondik+birlrk;
+//       System.out.println("Циврларынын косындысы: "+kosyndy+"\n"+"Цифрларының көбейтіндісі: "+juz*birlrk*ondik);
+
+//        8 Тапсырма
+//        int a = 378;
+//        int juz = a/100;
+//        int ondyk = a%100/10;
+//        int birlik = a%100%10;
+//        System.out.println("цифрларын оңнан солға қарай оқу арқылы жаңа санды құрастырыңыз:"+birlik+ondyk+juz );
+
+//        9 Тапсырма
+
+//        int a = 951;
+//        int s = a/100;
+//        int d = a%100/10;
+//        int z = a%100%10;
+//        System.out.println("1 сан "+s+z+d+"\n"+"2 Сан "+d+s+z+"\n"+"3 Сан");
+
+
+////        10 Тапсырма
+//        int q = 7536;
+//        int myn = q/1000;
+//        int juz = q%1000/100;
+//        int on = q%1000%100/10;
+//        int bir = q%10;
+//        int kosu = myn+juz+on+bir;
+//        int kobeitu = myn*juz*on*bir;
+//        System.out.println("Kosindisi: "+juz+"\n"+"Kobeitindisi: "+on);
+
+
+//        6 Lesson
+//        1 Тапсырма
+//       int x = 16;
+//       int y = 10;
+//
+//       if( x==16) {
+//           System.out.println("x is 16");
+//       }
+//       if( y != 11){
+//           System.out.println("y is not 11");
+//       }
+//        1 Есеп
+int a = 19;
+if( a>=18){
+    System.out.println("Автокөлік жүргізушісі куәлігін алуға өтініш бере алады");
+}
+// 2 есеп
+        int baqa = 4;
+if(baqa>2){
+    System.out.println("Келесі cыныпқа өтуге болады");
+}
+// 2 Тапсырма
+int x = 16;
+int y = 10;
+if(x>y){
+    System.out.println(x+" is bigger than "+y);
+}else
+    System.out.println(y+" is bigger than "+x);
+//3 есеп
+        int san_1 = 54;
+        int san_2 = 16;
+        if(san_1<san_2){
+            System.out.println(san_1+" киши "+san_2);
+        }else
+            System.out.println(san_2+" киши "+san_1);
+//3 Тапсырма
+
+    }
+    }
